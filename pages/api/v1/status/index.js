bf6648e72262d5203c238e85a486a90de878a615
kbrunot . <eukevinbruno@gmail.com>
2026-01-14 13:31:41 -0300

    atualização envolvendo variaveis de ambiente

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-const status = (request, response) => {
+import database from "../../../../infra/database.js";
+
+const status = async (request, response) => {
+  const result = await database.query("Select 2 + 2 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "valor" }); //.send("sem charset né");
 };
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,4 @@
-test("Status da API (200)", async () => {
+test("GET /api/v1/status deve retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
